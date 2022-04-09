748437acabc461f5aa05978e53327184265a0aa0
jusow <82551651+jusow@users.noreply.github.com>
2022-04-09 18:45:46 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
+      strings: ["Julia Sowers.", "Designer.", "Developer."],
       typeSpeed: 100,
       loop: true,
     });
